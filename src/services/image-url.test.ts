73545779258f3bd0c0479cdde97a345f575f598b
Mark Lester Echavez <marklester.echavez@gmail.com>
2023-12-18 13:15:52 +0800

    Test image url when empty

--- src/services/image-url.test.ts
+++ src/services/image-url.test.ts
@@ -1,6 +1,15 @@
 import getCroppedImageUrl from "./image-url";
 
 describe("image-url", () => {
+  it("should render a default image when image is not available", () => {
+    const url = "";
+    const testValue = "../assets/no-image.webp";
+
+    const croppedImageUrl = getCroppedImageUrl(url);
+
+    expect(croppedImageUrl).toEqual(testValue);
+  });
+
   it("should return the cropped image", () => {
     const url =
       "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg";
