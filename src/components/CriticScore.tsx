3980f431798ba71925e259b36e34f1149a9f6f2e
Mark Lester Echavez <marklester.echavez@gmail.com>
2023-12-18 09:55:13 +0800

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,15 @@
+import { Badge } from "@chakra-ui/react";
+
+type Props = {
+  score: number;
+};
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge colorScheme={color} fontSize="14px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
